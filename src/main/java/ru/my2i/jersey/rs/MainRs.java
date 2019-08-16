2c179dfc649e7747663298c2127afb0c3972a2d6
AndreySundukov <lulonik@mail.ru>
2019-08-16 15:33:12 +0300

    update and simplify

--- src/main/java/ru/my2i/jersey/rs/MainRs.java
+++ src/main/java/ru/my2i/jersey/rs/MainRs.java
@@ -4,8 +4,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.List;
 
 @Path("/main")
 public class MainRs {
@@ -13,7 +15,7 @@ public class MainRs {
     @GET
     @Path("/{param}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getParam(@PathParam("param") String param) {
-        return Response.ok().entity(param).build();
+    public List<String> getParam(@PathParam("param") String param, @QueryParam("p") String p) {
+        return Arrays.asList(param, p);
     }
 }
